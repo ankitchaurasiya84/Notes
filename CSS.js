938323aabe909c0429495e790658521bbfb1ee83
Ankit Chaurasiya <63976851+ankitchaurasiya84@users.noreply.github.com>
2025-03-22 11:47:35 +0530

    Update CSS.js

--- CSS.js
+++ CSS.js
@@ -13,3 +13,43 @@ Q6: what is flex box? Flexbox is a CSS layout module designed to provide a more 
 	even when their size is unknown or dynamic. It allows for responsive design and simplifies the process of creating complex layouts.
 
 Q7: casecading ~ specificity
+
+<Ankit> </Ankiit> inline
+By default: No, <Ankit> will behave like an inline element unless you style it with display: block;.
+
+# ==========box model============
+Padding: The space between the content and the border.
+Border: The border around the content and padding.
+Margin: The space outside the border
+
+Selector are pattern that are used to select and style the elements.
+
+id selector #, Class selector .,
+
+=================================z index====================================
+z-index is a property that controls the stacking order of elements on the z-axis.
+defualt value : auto
+Purpose: It determines which element appears on top when elements overlap.
+Elements with a higher z-index value will appear in front of elements with a lower z-index.
+
+Works with Positioned Elements: z-index only works on elements that have a position value other than static.
+This means the element must be positioned using position: relative, position: absolute, position: fixed, or position: sticky.
+
+==============================================================================================================
+
+Q: Pseudo-classes in CSS are used to define the special state of an element. They allow you to target an element when it is in a specific state or condition, such as when it is being hovered over, or when it has been visited.
+
+:hover
+:focus
+:active
+:visited
+:checked
+
+Pseudo-elements??
+Pseudo-elements in CSS are used to style specific parts of an element, such as the first letter, first line, or content inserted before or after an element.
+ie first leter and last letter
+Q: Grid vs Flexbox
+Grid: its 2D layout system that controls row and columns
+Flexbox: iyts a 1D layoutr system that arrange items either in row or cloumns
+
+Q: What is the difference between block, inline, and inline-block elements in CSS?
