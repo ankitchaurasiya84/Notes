ee2030cb524e48321a267095b56674a458d45e69
Ankit Chaurasiya <63976851+ankitchaurasiya84@users.noreply.github.com>
2025-03-26 00:43:41 +0530

    Update MachineCoding.js

--- MachineCoding.js
+++ MachineCoding.js
@@ -8,5 +8,13 @@
 2: Display data from array of Object to table using React JS
 3: intial red Lght 15sec yellow , green,
 4: railway ticket system
-5: Impliment pagination using DummyJSON api.
+5: Impliment pagination using DummyJSON api. [✅]
 6: impliment progress bar [dynamic]
+7: password Generator [✅]
+8: countdown timer basic [✅]
+9: countdown timer advance 
+10: File Uploader with Preview
+11: To-Do List with Local Storage
+12: Debounced Search Bar/  Debounced Search (API Integration)
+13: Light / Dark Theme Toggle
+14:E-Commerce Cart (Add/Remove Items)
